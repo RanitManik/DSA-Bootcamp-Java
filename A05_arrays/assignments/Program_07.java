e0e753eea341b557d659e6433785787f18fa776a
RanitManik <ranitmanikofficial@outlook.com>
2024-01-11 02:14:10 +0530

    added more programs

---
+++ A05_arrays/assignments/Program_07.java
@@ -0,0 +1,15 @@
+package A05_arrays.assignments;
+
+public class Program_07 {
+    public int numIdenticalPairs(int[] nums) {
+        int numGoodPairs = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if ((nums[i] == nums[j]) && (i < j)) {
+                    numGoodPairs++;
+                }
+            }
+        }
+        return numGoodPairs;
+    }
+}
---
+++ A05_arrays/assignments/Program_08.java
@@ -0,0 +1,28 @@
+package A05_arrays.assignments;
+
+import java.util.ArrayList;
+
+public class Program_08 {
+
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        ArrayList<Integer> listAnswer = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if ((j != i) && (nums[j] < nums[i])) {
+                    count++;
+                }
+            }
+            listAnswer.add(count);
+        }
+
+        // Convert ArrayList to array
+        int[] result = new int[listAnswer.size()];
+        for (int i = 0; i < listAnswer.size(); i++) {
+            result[i] = listAnswer.get(i);
+        }
+
+        return result;
+    }
+}
