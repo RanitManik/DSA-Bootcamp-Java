81625ba095da26c4e4f328dd1d43c208f9b29a25
RanitManik <ranitmanikofficial@outlook.com>
2024-01-27 18:47:18 +0530

    added more programs

---
+++ A05_arrays/assignments/Program_12.java
@@ -0,0 +1,16 @@
+package A05_arrays.assignments;
+
+public class Program_12 {
+    public int largestAltitude(int[] gain) {
+        int max = 0;
+        for (int i = 0; i < gain.length; i++) {
+            if (i != 0) {
+                gain[i] += gain[i - 1];
+            }
+            if (gain[i] > max) {
+                max = gain[i];
+            }
+        }
+        return max;
+    }
+}
