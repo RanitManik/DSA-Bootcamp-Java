156bae422f86266ed327f661d65838faabb1d361
RanitManik <ranitmanikofficial@outlook.com>
2024-02-11 19:12:53 +0530

    added more programs

--- 05. arrays/assignments/Program_25.java
+++ 05. arrays/assignments/Program_25.java
@@ -1,5 +1,3 @@
-package A05_arrays.assignments;
-
 public class Program_25 {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
         int m = mat.length;
---
+++ 05. arrays/assignments/Program_27.java
@@ -0,0 +1,18 @@
+public class Program_27 {
+
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int left = 0;
+
+        for (int right = 1; right < nums.length; right++) {
+            if (nums[right] != nums[left]) {
+                left++;
+                nums[left] = nums[right];
+            }
+        }
+        return left + 1;
+    }
+}
